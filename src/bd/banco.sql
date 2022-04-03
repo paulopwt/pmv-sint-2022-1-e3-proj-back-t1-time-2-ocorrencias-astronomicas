e62da314f2c74e515f6e720e1b360325df10082c
paulopwt <81271458+paulopwt@users.noreply.github.com>
2022-04-03 18:25:09 -0300

    Banco SQL

--- src/bd/banco.sql
+++ src/bd/banco.sql
@@ -1,1 +1,17 @@
+USE [app_web_backend]
+GO
 
+/****** Object: Table [dbo].[Dados] Script Date: 03/04/2022 17:13:53 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Dados] (
+    [Id]    NVARCHAR (450) NOT NULL,
+    [Nome]  NVARCHAR (MAX) NOT NULL,
+    [Email] NVARCHAR (MAX) NOT NULL,
+    [Senha] NVARCHAR (MAX) NOT NULL,
+    [CEP]   INT            NOT NULL
+);
